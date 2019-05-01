6282b246cc87d9ee8609bd0d59e45e89842e8ad5
Alvaro Silva <alvaro.facneto@gmail.com>
2019-05-01 12:23:04 -0300

    alteracoes sistema 0105

---
+++ br.com.livrariashalom/BLL/ItemVendaBLL.cs
@@ -0,0 +1,28 @@
+﻿using br.com.livrariashalom.DAO;
+using br.com.livrariashalom.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace br.com.livrariashalom.BLL
+{
+    public class ItemVendaBLL
+    {
+        private ItemVendaDAO itemVendaDAO = new ItemVendaDAO();
+
+        public void SalvarItem(ItemVenda itemVenda)
+        {
+            try
+            {
+                itemVendaDAO.SalvarItemVenda(itemVenda);
+            }
+            catch(Exception error)
+            {
+                MessageBox.Show("Erro: " + error);
+            }
+        }
+    }
+}
---
+++ br.com.livrariashalom/View/TelaItemVenda.xaml.cs
@@ -0,0 +1,69 @@
+﻿using br.com.livrariashalom.BLL;
+using br.com.livrariashalom.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace br.com.livrariashalom.View
+{
+    /// <summary>
+    /// Lógica interna para TelaItemVenda.xaml
+    /// </summary>
+    public partial class TelaItemVenda : Window
+    {
+        private ItemVendaBLL itemVendaBLL;
+
+        public TelaItemVenda()
+        {
+            InitializeComponent();
+        }
+
+        private bool SalvarItem(ItemVenda itemVenda)
+        {
+
+            try
+            {
+                //caso os campos estiverem vazios
+                if (txtQtd.Text == "" || txtSubTotal.Text == "")
+                {
+                    MessageBox.Show("Campos com * são obrigatórios o preenchimento");
+                }
+                else
+                {
+                    itemVenda.Quantidade = Convert.ToInt32(txtQtd.Text);
+                    itemVenda.Livro.CodLivro = Convert.ToInt32(txtCodLivro.Text);
+                    itemVenda.Produto.CodProduto = Convert.ToInt32(txtCodProduto.Text);
+                    itemVenda.SubTotal = Convert.ToDouble(txtSubTotal.Text);
+
+                    itemVendaBLL = new ItemVendaBLL();
+                    itemVendaBLL.SalvarItem(itemVenda);
+
+                    return true;
+                }
+
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Erro: " + error);
+            }
+            return false;
+
+        }
+
+        private void BtnPesquisarLivro_Click(object sender, RoutedEventArgs e)
+        {
+            TelaEstoque telaEstoque = new TelaEstoque();
+            telaEstoque.Show();
+        }
+    }
+}
